04d4377008dc5d57c2a6f9488041b89e04ea61dc
Adam Smith <adamsmith601@hotmail.com>
2016-05-27 20:43:38 -0400

    ViewState Example
    A short example for storing and retrieving values from a ViewState

---
+++ ViewState/ViewState.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewState.aspx.cs" Inherits="ViewState.ViewState" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txtValue" 
+                     runat="server" 
+                     Height="160px" 
+                     TextMode="MultiLine" 
+                     Width="460px">This is a test</asp:TextBox><br />
+        <br />
+        <asp:Button ID="cmdSave" 
+                    runat="server" 
+                    OnClick="cmdSave_Click" 
+                    Text="Save Contents" />
+        <asp:Button ID="cmdLoad" 
+                    runat="server" 
+                    OnClick="cmdLoad_Click" 
+                    Text="Load Contents" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ViewState/ViewState.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewState
+{
+    public partial class ViewState : System.Web.UI.Page
+    {
+        private string contents;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.IsPostBack)
+            {
+                contents = (string)ViewState["contents"];
+            }
+        }
+
+        protected void Page_PreRender(Object sender, EventArgs e)
+        {
+            ViewState["contents"] = contents;
+        }
+
+        protected void cmdSave_Click(object sender, EventArgs e)
+        {
+            contents = txtValue.Text;
+            txtValue.Text = "";
+        }
+        protected void cmdLoad_Click(object sender, EventArgs e)
+        {
+            txtValue.Text = contents;
+        }
+    }
+}
